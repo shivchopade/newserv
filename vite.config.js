cefd2202f4fe74b595efde6fa7c437139ebe8b58
SHIVAM CHOPADE <shivamvijaychopade@SHIVAMs-MacBook-Air.local>
2025-03-31 19:16:02 +0530

    third commit

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 // vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import process from 'process';
 const a = 'https://shiv7.pagekite.me/';
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
+    port: process.env.PORT || 5173,
     proxy: command === 'serve' ? {
       '/upload': {
         target: a,
